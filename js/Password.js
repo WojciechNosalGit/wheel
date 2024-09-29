36b12965237703dec3b0e5774e225ef53f2f1388
Wojciech Nosal <wojciechnosal.kontakt@gmail.com>
2024-09-29 18:24:22 +0200

    Passwords.json

--- js/Password.js
+++ js/Password.js
@@ -19,6 +19,7 @@ class Password {
 
   #randomNumber(max, min = 0) {
     const random = Math.floor(Math.random() * (max - min)) + min;
-    return 0;
+    console.log(random);
+    return random;
   }
 }
